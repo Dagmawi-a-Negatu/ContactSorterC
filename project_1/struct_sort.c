b8adcc24a2d479ded204dbab65af4293d981c34a
Dagmawi Negatu <dagmawi.negatu@gmail.com>
2024-02-17 22:14:03 -0500

    remove leading white spaces

--- project_1/struct_sort.c
+++ project_1/struct_sort.c
@@ -26,17 +26,38 @@ int main(int argc, char*argv[]){
     /**
      * Function to trim leading whitespace
      */
-    void trimTrailingWhitespace(char *line){
+    void trimLeadingWhitespace(char *line){
 
-        int i = 0;//index that allows to begin at the start of line.
+        //No leading whitesapces
+        if(!(isspace((unsigned char)line[0]))){
+
+            return ;
+        }
+
+        int originalLength = strlen(line);//Original length of array
+        int i = 0;//Intended for the first non whitespace character
         while ( isspace((unsigned char)line[i])){
             i++;
-        }
-        //Loop that checks if we are at a meaningful character
-        line[i - 1] = '\0';//Once we found the first charcater,
-        //remove the leading whitespace by the null \O char representation
+        }//Loop that checks if at non white space  character
+        
+        
+        int j = 0;// J used to iterate over each index in line
+        int num_shifts = i;//number of shifts to take to the left
+        int j_bound = (originalLength - 1) - num_shifts;//Index of last character
+        //Shift elements to the left removing leading white spaces
+        while(j < (j_bound)){
+            
+            //Shift every character by number of leading whitespaces
+            line[j] = line[j + num_shifts];
+            j++;
 
-     }
+
+        }//Left with trailing white spaces equivalent to number of 
+        //leading white spaces with no terminating null character
+        
+        //Remove trailing white spaces with terminating null character
+        line[j] = '\0';
+        }
 
     //Buffer holds each line of multiple characters from input file 
     while(fgets(buffer, sizeof(buffer), filePointer != NULL){
