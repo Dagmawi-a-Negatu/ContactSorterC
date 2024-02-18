55a74ab56227d94d63de582ad41d10c265e6d672
Dagmawi Negatu <dagmawi.negatu@gmail.com>
2024-02-17 20:02:38 -0500

    required libraries, whiteLeadingspace function

--- project_1/struct_sort.c
+++ project_1/struct_sort.c
@@ -1,12 +1,13 @@
 #include "struct_sort.h";
 #include <stdio>;
-
+#include <ctype.h>
+#include <stdlib.h>
 
 int main(int argc, char*argv[]){
 
-    Person data[NUM_PEOPLE];// Holds arrau of structs that represent each person.
+    Person data[NUM_PEOPLE];// Holds array of structs that represent each person.
     File *filePointer; // A pointer to a file intended for the file input.
-    char buffer[1024];
+    char buffer[256];// buffer that holds a singe line at a time.
 
     //Check if the correct number of arguments passed is correct
     if(argc != 3){
@@ -17,13 +18,31 @@ int main(int argc, char*argv[]){
 
     //Attempt to open the input file
     filePointer  = fopen(argv[1], "r");
-    if (inputFile == NULL) {
+    if (filePointer == NULL) {
         perror("Error opening input file");
         return EXIT_FAILURE;
     }
 
+    /**
+     * Function to trim leading whitespace
+     */
+    void trimTrailingWhitespace(char *line){
+
+        int i = 0;//index that allows to begin at the start of line.
+        while ( isspace((unsigned char)line[i])){
+            i++;
+        }
+        //Loop that checks if we are at a meaningful character
+        line[i - 1] = '\0';//Once we found the first charcater,
+        //remove the leading whitespace by the null \O char representation
+
+     }
 
     //Buffer holds each line of multiple characters from input file 
     while(fgets(buffer, sizeof(buffer), filePointer != NULL){
-
+        
+        trimLeadingWhitespace(buffer);
+        //Use this assuming we have no white leading spaces
+        sscanf(buffer, "%s", "%s", "%d" "%s" "%s", "%s", 
+        "%s", "%d", "%f", 
 
