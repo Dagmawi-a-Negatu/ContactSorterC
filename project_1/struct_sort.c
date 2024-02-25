919c2220e83bea27b757dafcb716a26c29994bf1
Nick Buchanan <git@github.com:NickA821/Ciphers.git>
2024-02-24 21:10:12 -0500

    Finished documentation for main function

--- project_1/struct_sort.c
+++ project_1/struct_sort.c
@@ -3,7 +3,17 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(int argc, char*argv[]) {
+
+/**
+ * Driver function of the program. Instantiates Person struct array and buffer to hold each
+ * line of input. Each line is read into the buffer and trimmed of leading whitespace. Data in the buffer is then
+ * assigned to respective fields in the struct array for each line given by the input file. Once the struct array has been
+ * filled the program sorts it using a bubble sort and writes the contents to a file
+ * @param int argc: Number of arguments provided to the program
+ * @param char *argv[]: List of arguments provided to the program
+ * @return 0: Indicates successful termination of program
+*/
+int main(int argc, char *argv[]) {
 
     Person data[NUM_PEOPLE];        // Holds array of structs that represent each person.
     char buffer[256];       // Buffer that holds a single line at a time.
@@ -15,18 +25,26 @@ int main(int argc, char*argv[]) {
     // Attempt to open the input file
     FILE *filePointer = openFile(argv);     
  
-    // While 
+    // While the buffer has a line from the file in it
     while (fgets(buffer, sizeof(buffer), filePointer) != NULL) {
-        //Clearing all clear spaces with null terminating character
+        // Clear all whitespace at the beginning of each line
         trimLeadingWhitespace(buffer);
-        //Write the contents of buffer to each person record
+        //Write the contents of the buffer to each Person record
         readFields(buffer, data, &size);
     }
+
     // Use bubble sort to organize data based on last name/first name if last names are equal
     bubbleSort(data, size);     
+
+    // Write sorted data to file specified by program arguments
     writeToFile(data, size, argv);
+    // return 0 indicating successful termination of program
+    return 0;
 }
 
+/**
+ * Returns a pointer to the contents of a file
+*/
 FILE* openFile(char* argv[]) {
     FILE *filePointer;      // A pointer to a file for the file input.
     filePointer  = fopen(argv[1], "r");
